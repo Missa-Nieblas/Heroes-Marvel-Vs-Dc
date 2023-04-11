503662a331bf42313924ba511b7d5b246d3f61c0
Missa <missa_nieblas@hotmail.com>
2023-04-11 00:01:04 -0700

    Se Agregaron clases HeroCard, getHeroById, HeroList, todo eso para poder ver las tarjetas de los heroes y su informacion en ellas, botton back, etc.

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,15 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+    const onLogout = () =>{
+        navigate('/login',{
+            replace: true
+        });
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -40,7 +48,9 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                    <span className='nav-item nav-link text-info'>Tommy</span>
-                    <button className='nav-item nav-link btn'>Logout</button>
+                    <button className='nav-item nav-link btn'
+                    onClick={onLogout}
+                    >Logout</button>
                 
                 </ul>
             </div>
